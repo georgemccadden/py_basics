30f50e22087477deda21ef26edfc011fdf7898ba
George McCadden III <thethirdone.g@gmail.com>
2019-06-24 19:31:45 -0700

    Reviewing lists and the slice method

---
+++ list_practice.py
@@ -0,0 +1,8 @@
+my_list = ['George', 'Miguel', 'Debbie', 25, 22, 62, True, True, False]
+
+# Practicing the slice() function on a list.
+
+names = my_list[0:3] # This creates another list which holds the first three items, the names. The first parameter is the starting point (it's default is the beginning of the list). The second paremeter is where to stop (and it does not include that index - so it stops right before index 3).
+
+print(names) # This should print 'George', 'Miguel', and 'Debbie'.
+
