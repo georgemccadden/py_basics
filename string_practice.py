f31782ecf309ad922add7b055f2315573b01a05c
George McCadden III <thethirdone.g@gmail.com>
2020-09-20 13:36:04 -0700

    Creating some consistency with these quotation marks

--- string_practice.py
+++ string_practice.py
@@ -6,25 +6,25 @@ print(hello)
 
 name = input("What is your name? : ")
 
-print('It is so nice to meet you')
+print("It is so nice to meet you")
 
 # Ask the user for their age
 
-age = input('So.. tell me about yourself. How old are you? : ')
+age = input("So.. tell me about yourself. How old are you? : ")
 
-print('Wow, that old?')
+print("Wow, that old?")
 
 # Ask the user what city they grew up in
 
-city = input('''You don't seem like you're from around here. Where did you grow up? : ''')
+city = input("You don't seem like you're from around here. Where did you grow up? : ")
 
 # Ask the user what they enjoy
 
-enjoy = input('''I've never been there. What kinds of things do you like to do? : ''')
+enjoy = input("I've never been there. What kinds of things do you like to do? : ")
 
 # Create the output text
 
-string = 'Your name is {} and you are {} years old. You grew up in {}. Oh and by the way, you love {}.'
+string = "Your name is {} and you are {} years old. You grew up in {}. Oh and by the way, you love {}."
 output = string.format(name, age, city, enjoy)
 
 # Print the output to the screen
